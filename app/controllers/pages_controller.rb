58855f1611e0f2fa0f3a40f7e2fe5310998a0bca
Stilyan Dimitrov <sti4o_21@aol.com>
2025-05-04 17:12:11 -0500

    cars page done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,28 @@
 class PagesController < ApplicationController
 
-    def about 
-        @user = User.find_by(id: 1)
+    def about
         @about = @user.bio
         render :about
     end 
 
     def contact
-        @user = User.find_by(id: 1)
         @phone = @user.phone.gsub(/(\d{3})(\d{3})(\d{4})/, '(\1) \2-\3')
 
         render :contact
     end
     
     def policy
-        @user = User.find_by(id: 1)
         @policy = @user.policy
         render :policy
     end
 
+    def cars 
+        @cars = Car.all
+        render :cars
+    end
+
+    def car
+        @car = Car.find(params[:car_id])
+        render :car
+    end
 end
