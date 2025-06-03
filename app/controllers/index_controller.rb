f8d7a50040ce9478a1bf713d22828b5919857a0e
Stilyan Dimitrov <sti4o_21@aol.com>
2025-06-03 13:12:39 -0500

    removed featured

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,8 +3,6 @@ class IndexController < ApplicationController
     def page
         @user = User.find(1)
         @car = Car.order(:id).first
-        @feature = Car.order(:id).first.images.order(:created_at).first
-
         render :index
     end 
 
